ab331b5d9013950a28bbdd67c9a47b474cc9a611
Yusuf Atalar <yusuf.atalar@iottech.com.tr>
2023-02-18 18:49:42 +0300

    username regex

---
+++ src/strings/introduction/TagContentExtractor.java
@@ -0,0 +1,27 @@
+package src.strings.introduction;
+
+import java.util.Scanner;
+
+public class TagContentExtractor {
+
+  public static void main(String[] args) {
+
+    Scanner in = new Scanner(System.in);
+    int testCases = Integer.parseInt(in.nextLine());
+    while (testCases > 0) {
+      String line = in.nextLine();
+
+      //Write your code here
+
+      testCases--;
+    }
+  }
+}
+/*
+<h1>Nayeem loves counseling</h1>
+<h1><h1>Sanjay has no watch</h1></h1><par>So wait for a while</par>
+<Amee>safat codes like a ninja</amee>
+<SA premium>Imtiaz has a secret crush</SA premium>
+ */
+
+
